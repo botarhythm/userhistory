796e62e0fb53d4e89ca946669fe9ddf14c2b71d3
botarhythm <info@botarhythm.com>
2025-08-01 14:56:23 +0900

    fix: listenポートをprocess.env.PORT || 3000に修正

--- test-server/server.ts
+++ test-server/server.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.get('/api/hello', (_req, res) => {
   res.json({ message: 'Hello, Railway!' });
