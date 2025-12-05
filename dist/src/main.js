1b174d0484026358271c4930d07a9b462b8be8a8
botarhythm <info@botarhythm.com>
2025-12-05 14:13:17 +0900

    chore: Remove dist from git to force Railway build

--- dist/src/main.js
+++
@@ -1,6 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./styles/tailwind.css";
-ReactDOM.createRoot(document.getElementById("root")).render(_jsx(App, {}));
-//# sourceMappingURL=main.js.map
